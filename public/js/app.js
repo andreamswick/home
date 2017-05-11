f59a1d85d2cacbca95e8cf5e7a0cbdf1c048a42d
Andrea Swick <andreamswick@gmail.com>
2017-05-10 20:31:20 -0500

    Move styles into components and add button styles

--- public/js/app.js
+++ public/js/app.js
@@ -48226,7 +48226,7 @@ module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c
   }, [_c('div', {
     staticClass: "col-md-6"
   }, [_c('button', {
-    staticClass: "btn btn-default btn-block",
+    staticClass: "btn btn-outline-white btn-block",
     on: {
       "click": function($event) {
         _vm.start(25)
@@ -48235,7 +48235,7 @@ module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c
   }, [_vm._v("25 minutes")])]), _vm._v(" "), _c('div', {
     staticClass: "col-md-6"
   }, [_c('button', {
-    staticClass: "btn btn-default btn-block",
+    staticClass: "btn btn-outline-white btn-block",
     on: {
       "click": function($event) {
         _vm.start(5)
